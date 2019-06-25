779b6e2f0ebb5df61136c2781c04b71161f08354
2daysweb <sahnunhm@gmail.com>
2019-06-25 17:00:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+let form = document.getElementById("create-task-form")
+
+let userInput = document.getElementById("new-task-description")
+
+
+
+
+form.addEventListener("submit", function(){
+  event.preventDefault()
+
+  let li = document.createElement("li")
+  let textnode = document.createTextNode(userInput.value)
+  li.appendChild(textnode)
+
+  document.getElementById("tasks").appendChild(li)
+  console.log(document.getElementById("tasks"))
+  
+}
+)
