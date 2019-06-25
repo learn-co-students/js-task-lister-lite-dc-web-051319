e628cbd2d57d2460e22c885c5f9c2382c1ac2cfc
Dan Kagan <dankagan7@gmail.com>
2019-06-25 16:48:41 -0400

    can add tasks to list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+let form = document.getElementById("create-task-form")
+let input = document.getElementById("new-task-description")
+let ul = document.getElementById("tasks")
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener("submit", createTask)
+
 });
+
+  // waiting for Submit input, then running createTasks
+
+
+
+function createTask(event) {
+  event.preventDefault();
+
+  console.log(event);
+
+  let li = document.createElement("li");
+  li.innerText = input.value;
+
+  ul.appendChild(li)
+
+  console.log(ul)
+
+  console.log(li)
+}
