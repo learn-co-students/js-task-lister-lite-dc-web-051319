9afab559110a2187c5424473102ef3fe40234305
Meghan Miller <mille265@gmail.com>
2019-06-25 16:53:20 -0400

    things

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //grab all the necessary DOM elements
+  
+    //form and relevant input fields
+    const newTaskForm = document.getElementById("create-task-form");
+    const newTaskDescription = document.getElementById("new-task-description");
+
+    const newTaskUl = document.getElementById("tasks");
+}
