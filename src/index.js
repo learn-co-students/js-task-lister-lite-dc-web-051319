25be29e8aaaaeacc4c29be147cbf6a11c46c60d6
Natany Caxeta <natanycaxeta@icloud.com>
2019-06-25 17:11:05 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
+
+let form = document.getElementById("create-task-form")
+let userInput = document.getElementById("new-task-description")
+let ul = document.getElementById("tasks")
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  form.addEventListener("submit", function(){
+    event.preventDefault()
+
+    let li = document.createElement("li")
+    li.innerText = userInput.value
+    // let textnode = document.createTextNode(userInput.value)
+    ul.appendChild(li)
+    console.log(document.getElementById("tasks"))
+
+  })
+})
