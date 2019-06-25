11d5c1d01523769832614205d00b705007e9afb0
villatrue <scarter0313@gmail.com>
2019-06-25 17:26:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const taskField = document.getElementById("new-task-description")
   const submitButton = document.querySelector("input[type = 'submit']")
   const taskList = document.getElementById("tasks")
+  const taskPriority = document.getElementById("new-task-priority")
+
   submitButton.addEventListener('click', newListItem)
   
 
 
   function newListItem(event){
     event.preventDefault();
+    
 
-    // if !taskField.value{
+    if (!!taskField.value){
       let newId = taskList.childElementCount +1;
+      let priority = taskPriority.value
 
       let li = document.createElement("li");
       li.dataset.id = newId;
+      li.dataset.priority = priority;
       li.innerText = taskField.value;
       taskList.appendChild(li);
 
@@ -23,7 +28,10 @@ document.addEventListener("DOMContentLoaded", () => {
       deleteButton.innerText = " x ";
       li.appendChild(deleteButton);
       deleteButton.addEventListener('click', deleteItem);
-    // }
+    } else {
+      alert("add somethin ya fool")
+    }
+   
   }
   function deleteItem(e) {
     e.stopPropagation
